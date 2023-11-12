3a8611db84453b79f5040fab6c8129cc0f716683
basil <basilreji3@gmail.com>
2023-11-12 14:08:36 +0530

    updated URL

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,10 +1,8 @@
 export const getBaseURL = () => {
-  console.error(window, "Window value");
-
   if (typeof window !== "undefined") {
     return window.location.origin;
   } else {
     // When not in the browser environment (server-side), you can provide a default URL
-    return "http://localhost:3000"; // Replace with your default URL
+    return process.env.BASE_URL; // Replace with your default URL
   }
 };
